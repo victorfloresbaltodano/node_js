30a51eef2736c0d7232c026358c63d90c86b8f08
vflores <vflores@poderjudicial.gob.ni>
2022-05-05 10:49:15 -0600

    Routing

--- vk-store/index.js
+++ vk-store/index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello from my server.');
 })
 
+app.get('/new-endpoint', (req, res) => {
+  res.send('Hello, I am a new end point.');
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'product 1',
+    price: 1000
+  });
+})
+
 app.listen(port, () => {
   console.log('Port: ', port);
 })
