a04a1dd4c006fd6822f5c19bc92e916007fee1ff
vflores <vflores@poderjudicial.gob.ni>
2022-05-05 10:33:52 -0600

    Primer servidor http

--- vk-store/index.js
+++ vk-store/index.js
@@ -1,1 +1,11 @@
-console.log('my app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my server.');
+})
+
+app.listen(port, () => {
+  console.log('Port: ', port);
+})
