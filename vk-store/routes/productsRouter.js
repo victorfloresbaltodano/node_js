adbe786598ee77589e6eb2efa8b677bf9990bd89
vflores <vflores@poderjudicial.gob.ni>
2022-05-12 11:08:25 -0600

    capa servicios

--- vk-store/routes/productsRouter.js
+++ vk-store/routes/productsRouter.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const productsService = require('./../services/productsService');
 
 const router = express.Router();
+const service = new productsService();
 
 router.get('/', (req, res) => {
-
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-
-  for(let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-
+  const products = service.find();
   res.json(products);
 })
 
@@ -27,18 +16,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  //const id = req.params.id;
-  if(id ==='999') {
-    res.status(404).json({
-      message: 'not found'
-    });
-  } else {
-    res.status(200).json({
-      id,
-      name: 'product 2',
-      price: 5000
-    });
-  }
+  const product = service.findOne(id);
+  res.json(product);
 })
 
 router.post('/', (req, res) => {
